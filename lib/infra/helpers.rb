4d550541cc1f0802dff048dba59cff8fe0207081
Tomas Svarovsky <svarovsky.tomas@gmail.com>
2012-06-06 14:56:47 +0200

    Truncate should take basedir in helper

--- lib/infra/helpers.rb
+++ lib/infra/helpers.rb
@@ -54,7 +54,7 @@ module Infra
           :es_name => es_name,
           :entity => entity,
           :timestamp => from,
-          :load_filenames => basedir
+          :basedir => basedir
         })
       else
         logger.warn "Variable LAST_FULL_RUN_START not filled in not truncating"
